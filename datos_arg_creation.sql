a74098d0ef8e61c6be4bd8c819581d1574b80408
slash-w <84292251+slash-w@users.noreply.github.com>
2022-10-15 23:47:42 -0300

    Add files via upload

---
+++ datos_arg_creation.sql
@@ -0,0 +1,13 @@
+-- Database: datosCulturaArg
+
+-- DROP DATABASE IF EXISTS "datosCulturaArg";
+
+CREATE DATABASE "datosCulturaArg"
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'Spanish_Argentina.utf8'
+    LC_CTYPE = 'Spanish_Argentina.utf8'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
